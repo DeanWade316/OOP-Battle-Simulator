187a7f607d0887394ad1d2700ec1e07f31edbd44
DeanWade316 <wadedean316@gmail.com>
2026-09-11 08:44:16 -0500

    New Goblin fixed

--- game.py
+++ game.py
@@ -14,7 +14,7 @@ def main():
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
 
     secondGoblin = Goblin("Scribble")
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{secondGoblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
 
 
