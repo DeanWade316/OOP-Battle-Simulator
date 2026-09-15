424f7b52c11cf7147c0248c8814a7a44b26349ed
DeanWade316 <wadedean316@gmail.com>
2026-09-15 10:35:43 -0500

    Build Hero Class

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 from goblin import Goblin
+from hero import Hero
 
 
-ARENA_NAME = "The Iron Lung"
+ARENA_NAME = "The Ring of Fire"
 
 
 def main():
@@ -10,10 +11,20 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
-
+    goblin = Goblin("Frank")
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    print("But no hero has answered the call... yet.")
+
+    secondGoblin = Goblin("Scribble")
+    print(f"{secondGoblin.name} enters the arena with {goblin.health} health.")
+
+    hero = Hero("Bob")
+    print(f"{hero.name} the {hero.hero_class} enters the arena with {hero.health} health.")
+
+    heroAttack = hero.attack()
+    goblin.take_damage(heroAttack)
+
+    goblinAttack = goblin.attack()
+    hero.take_damage(goblinAttack)
 
 
 if __name__ == "__main__":
